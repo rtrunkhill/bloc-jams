a39b6da21371a785a8d4763a11edb2491c6c27ed
rtrunkhill <rtrunkhill@gmail.com>
2017-06-12 10:11:24 -0700

    started cp9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,22 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-  var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
+var revealPoint = function(index)  {
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)";
+    points[index].style.msTransform = "scaleX(1) translateY(0)";
+    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+  }
 
-  var revealFirstPoint = function() {
-    points[0].style.opacity = 1;
-    points[0].style.transform = "scaleX(1) translateY(0)";
-    points[0].style.msTransform = "scaleX(1) translateY(0)";
-    points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-
-  var revealSecondPoint = function() {
-    points[1].style.opacity = 1;
-    points[1].style.transform = "scaleX(1) translateY(0)";
-    points[1].style.msTransform = "scaleX(1) translateY(0)";
-    points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-
-  var revealThirdPoint = function() {
-    points[2].style.opacity = 1;
-    points[2].style.transform = "scaleX(1) translateY(0)";
-    points[2].style.msTransform = "scaleX(1) translateY(0)";
-    points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-
-  revealFirstPoint();
-  revealSecondPoint();
-  revealThirdPoint();
-
+for (var i = 0; i < points.length; i++) {
+  revealPoint(i);
 };
+
+window.onload = function() {
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+
+  window.addEventListener('scroll', function(event) {
+        console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+   });
+}
+}
