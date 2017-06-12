412b2d91e07ccda4edf290923e0a4cb849091711
rtrunkhill <rtrunkhill@gmail.com>
2017-06-12 13:08:53 -0700

    cp 9 assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -16,7 +16,7 @@ window.onload = function() {
   if (window.innerHeight > 950) {
     animatePoints(pointsArray);
   }
-  
+
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
